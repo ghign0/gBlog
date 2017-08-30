69c9a53f093419189449f98ae327d4c138aff53b
ghign0-desktop <ghinassi.marco@gmail.com>
2017-08-31 01:38:39 +0200

    implementazione grafica nuovo progett

--- src/GBlogBundle/Form/PostType.php
+++ src/GBlogBundle/Form/PostType.php
@@ -5,6 +5,7 @@ namespace GBlogBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class PostType extends AbstractType
 {
@@ -13,7 +14,29 @@ class PostType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('slug')->add('creationDate')->add('status')->add('publishDate')->add('content')->add('user')->add('category')->add('tags');
+        $builder
+        ->add('title', null, [
+            'attr' => [
+                'placeholder' => 'inserisci il titolo del post'
+            ]
+        ])
+        ->add('slug', null, [
+            'attr' => [
+                'placeholder' => 'inserisci lo slug per URL'
+            ]
+        ])
+        ->add('category')
+        ->add('creationDate')
+        ->add('status', ChoiceType::class, [
+            'choices' => [
+                'bozza' => 0,
+                'pubblicato' => 1
+            ]
+        ])
+        ->add('publishDate')
+        ->add('content')
+        ->add('user')
+        ->add('tags');
     }
 
     /**
